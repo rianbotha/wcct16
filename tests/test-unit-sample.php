82c2eef5e9e48abee2bb2dfd2f20cdf745bfe4bb
Rian Botha <rian.botha@dreamsmiths.com>
2016-09-08 11:55:17 +0200

    Extended test

--- tests/test-unit-sample.php
+++ tests/test-unit-sample.php
@@ -11,10 +11,18 @@
 class UnitTests extends \PHPUnit_Framework_TestCase {
 	use PHPMock;
 
+	public function test_wcct_get_option() {
+		$get_option = $this->getFunctionMock('WCCT','get_option');
 
+		$get_option->expects($this->once())
+			->with($this->eqialTo('wcct_venue'))
+			->willReturn('river-club');
+	}
 
 	public function test_wcct_get_option() {
 		// Replace this with some actual testing code.
-		$this->assertEquals( 'river-club', wcct_get_option('venue') );
+		$this->assertEquals( 'river-club', wcct16_get_option('venue') );
 	}
+
+
 }
