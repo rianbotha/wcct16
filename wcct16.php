0ea5e0d7a59269922246aceb4a7b729d120148f6
Rian Botha <rian.botha@dreamsmiths.com>
2016-09-08 11:34:06 +0200

    Text change

--- wcct16.php
+++ wcct16.php
@@ -4,7 +4,7 @@ Plugin Name: WordCamp Cape Town 2016 - Continuous Integration
 Plugin URI:  https://2016.capetown.wordcamp.org/session/a-basic-introduction-to-continuous-integration-with-wordpress/
 Description: A nice little demo of absolutely nothing to achieve everything.
 Version:     0.5
-Author:      Seagyn Davis
+Author:      Rian Botha
 Author URI:  http://www.seagynsdavis.com
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
